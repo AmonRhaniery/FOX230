f298397c7d9b2749ef152f8025f49ed461d50bb0
Amon <amonrhaniery27@gmail.com>
2019-04-18 19:12:00 -0300

    release

---
+++ release/classes/scripts/learn.sh
@@ -0,0 +1,3 @@
+#nohup java -Xmx8G -cp fox-2.3.0-jar-with-dependencies.jar org.aksw.fox.FoxCLI -lde -atrain -iinput/Wikiner/aij-wikiner-de-wp3.bz2 > learn.log &
+nohup java -Xmx8G -cp fox-2.3.0-jar-with-dependencies.jar org.aksw.fox.FoxCLI -len -atrain -iinput/2 > learn.log &
+
---
+++ release/classes/scripts/run.sh
@@ -0,0 +1,1 @@
+nohup java -Xmx8G -cp fox-2.3.0-jar-with-dependencies.jar org.aksw.fox.FoxRESTful > run.log &
