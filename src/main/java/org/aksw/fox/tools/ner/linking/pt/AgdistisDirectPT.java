a0b0c5e98a623418d553fca16d30d1b64fd58049
Amon <amonrhaniery27@gmail.com>
2019-06-04 13:37:36 -0300

    fix

--- src/main/java/org/aksw/fox/tools/ner/linking/pt/AgdistisDirectPT.java
+++ src/main/java/org/aksw/fox/tools/ner/linking/pt/AgdistisDirectPT.java
@@ -1,9 +1,9 @@
-package org.aksw.fox.tools.linking.pt;
+package org.aksw.fox.tools.ner.linking.pt;
 
 import java.io.IOException;
 
 import org.aksw.agdistis.algorithm.NEDAlgo_HITS;
-import org.aksw.fox.tools.linking.common.Agdistis;
+import org.aksw.fox.tools.ner.linking.common.Agdistis;
 
 public class AgdistisDirectPT extends Agdistis {
 
